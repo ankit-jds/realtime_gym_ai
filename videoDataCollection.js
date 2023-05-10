26962d1300f1be7907270a06f253d7539a6be43a
ankit-jds <84713907+ankit-jds@users.noreply.github.com>
2023-05-10 15:47:45 +0530

    SINGLE pre recorded video perfect

--- videoDataCollection.js
+++ videoDataCollection.js
@@ -2,36 +2,53 @@ let video;
 let posenet;
 let singlePose, skeleton;
 let state = "collecting";
+let video_variables = {};
+let brain;
 
 function preload() {
   video = createVideo("./videos/New folder/1003_squat_000152.mp4", onVideoLoad);
   video.hide();
+  video.elt.addEventListener("loadeddata", startVideoPlayback);
 }
 
 function onVideoLoad() {
   console.log("Video metadata loaded.");
   console.log("Video width: " + video.width);
   console.log("Video height: " + video.height);
-  // Play the video
-  video.play();
+
+  video_variables["new_width"] = video.width * (720 / video.height);
+  video_variables["new_height"] = 720;
+
+  video_variables["x_ratio"] = video_variables["new_width"] / video.width;
+  video_variables["y_ratio"] = video_variables["new_height"] / video.height;
 
   posenet = ml5.poseNet(video, modelLoaded);
   posenet.on("pose", receivedPoses);
-}
 
-function setup() {
-  createCanvas(1280, 720);
+  // video.play();
+
+  // // Play the video
+  if (video_variables["video_loaded"]) {
+    console.log("VIDEO PLAYING");
+    video.play();
+  } else {
+    console.log("wait");
+  }
 
   video.onended(function () {
     brain.saveData();
+    console.log("VIDEO ENDED");
   });
-  // video.addEventListener("loadeddata", function () {
-  //   // Play the video
-  //   video.play();
+}
 
-  //   posenet = ml5.poseNet(video, modelLoaded);
-  //   posenet.on("pose", receivedPoses);
-  // });
+function startVideoPlayback() {
+  console.log("Video data loaded.");
+  video_variables["video_loaded"] = true;
+}
+
+function setup() {
+  console.log("SETUP STARTED");
+  createCanvas(1280, 720);
 
   let options = {
     inputs: 34,
@@ -44,7 +61,6 @@ function setup() {
 
 function receivedPoses(poses) {
   // console.log(poses);
-
   if (poses.length > 0) {
     singlePose = poses[0].pose;
     skeleton = poses[0].skeleton;
@@ -68,33 +84,34 @@ function modelLoaded() {
 
 function draw() {
   // Resize the video to fit a specific height while maintaining its aspect ratio
-  let video_height = video.height;
-  let video_width = video.width;
-  let new_width = video_width * (720 / video_height);
-  let new_height = 720;
+
   background(255, 0, 0);
-  image(video, 0, 0, new_width, new_height);
+  image(
+    video,
+    0,
+    0,
+    video_variables["new_width"],
+    video_variables["new_height"]
+  );
 
   fill(255, 0, 0);
 
-  let x_ratio = new_width / video_width;
-  let y_ratio = new_height / video_height;
   if (singlePose) {
-    console.log(singlePose, skeleton, "pose ");
+    // console.log(singlePose, skeleton, "pose ");
     for (let i = 0; i < singlePose.keypoints.length; i++) {
       if (singlePose.keypoints[i].score > 0.6) {
         ellipse(
-          singlePose.keypoints[i].position.x * x_ratio,
-          singlePose.keypoints[i].position.y * y_ratio,
-          10 * y_ratio
+          singlePose.keypoints[i].position.x * video_variables["x_ratio"],
+          singlePose.keypoints[i].position.y * video_variables["y_ratio"],
+          10 * video_variables["y_ratio"]
         );
       }
     }
     let leftAnkleX = singlePose.leftAnkle.x;
     let leftAnkleY = singlePose.leftAnkle.y;
-    let leftHipX = singlePose.leftHip.y;
+    let leftHipX = singlePose.leftHip.x;
     let leftHipY = singlePose.leftHip.y;
-    let leftKneeX = singlePose.leftKnee.y;
+    let leftKneeX = singlePose.leftKnee.x;
     let leftKneeY = singlePose.leftKnee.y;
 
     let angle = calcAngle(
@@ -117,10 +134,10 @@ function draw() {
 
     for (let j = 0; j < skeleton.length; j++) {
       line(
-        skeleton[j][0].position.x * x_ratio,
-        skeleton[j][0].position.y * y_ratio,
-        skeleton[j][1].position.x * x_ratio,
-        skeleton[j][1].position.y * y_ratio
+        skeleton[j][0].position.x * video_variables["x_ratio"],
+        skeleton[j][0].position.y * video_variables["y_ratio"],
+        skeleton[j][1].position.x * video_variables["x_ratio"],
+        skeleton[j][1].position.y * video_variables["y_ratio"]
       );
     }
 
@@ -131,23 +148,23 @@ function draw() {
 
 function calcAngle(x1, y1, x2, y2, x3, y3) {
   // Calculate the vectors between the points
-  const v1x = x1 - x2;
-  const v1y = y1 - y2;
-  const v2x = x3 - x2;
-  const v2y = y3 - y2;
+  let v1x = x1 - x2;
+  let v1y = y1 - y2;
+  let v2x = x3 - x2;
+  let v2y = y3 - y2;
 
   // Calculate the dot product of the vectors
-  const dotProduct = v1x * v2x + v1y * v2y;
+  let dotProduct = v1x * v2x + v1y * v2y;
 
   // Calculate the magnitudes of the vectors
-  const v1mag = Math.sqrt(v1x * v1x + v1y * v1y);
-  const v2mag = Math.sqrt(v2x * v2x + v2y * v2y);
+  let v1mag = Math.sqrt(v1x * v1x + v1y * v1y);
+  let v2mag = Math.sqrt(v2x * v2x + v2y * v2y);
 
   // Calculate the cosine of the angle between the vectors
-  const cosAngle = dotProduct / (v1mag * v2mag);
+  let cosAngle = dotProduct / (v1mag * v2mag);
 
   // Convert the cosine to an angle in degrees
-  const angleDeg = (Math.acos(cosAngle) * 180) / Math.PI;
+  let angleDeg = (Math.acos(cosAngle) * 180) / Math.PI;
 
   return angleDeg;
 }
